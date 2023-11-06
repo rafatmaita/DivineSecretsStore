e8c58c7310a9226c6ba6df30635fc0a7f5e5093f
rafatmaita <2030684@std.hu.edu.jo>
2023-11-06 04:49:37 +0300

    this final of final

--- views/view/src/pages/Profile.jsx
+++ views/view/src/pages/Profile.jsx
@@ -1,118 +1,92 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import OrderHistory from './OrderHistory';
-
+import axios from 'axios';
 
 const Profile = () => {
-  const [userId = 1, setUserId] = useState();
-  const [userData, setUserData] = useState({
-    username: '',
-    email: '',
-  });
-
+  const { userId = 1 } = useParams();
+  const [userData, setUserData] = useState(null);
+  const [activeTab, setActiveTab] = useState('EditProfile');
   const [newUserData, setNewUserData] = useState({
     username: '',
     email: '',
     password: '',
   });
 
-  const [userImage, setUserImage] = useState('');
-
-  const [activeTab, setActiveTab] = useState('EditProfile');
-
-  const fetchUserData = () => {
-    axios.get(`http://localhost:5002/user/${userId}`)
-      .then(response => {
-        setUserData(response.data);
-        setNewUserData({ username: response.data.username, email: response.data.email });
+  const handleSaveChanges = () => {
+    console.log('Save button clicked');
+    axios
+      .put(`http://localhost:5002/user/${userId}`, newUserData, {
+        // headers: {
+        //   'Content-Type': 'application/json',
+        // },
       })
-      .catch(error => {
-        console.error('Error fetching user data: ', error);
-      });
-
-    axios.get(`http://localhost:4000/user/${userId}`)
-      .then(response => {
-        setUserImage(`http://localhost:4000/${response.data.image}`);
+      .then((response) => {
+        console.log('success', response.data);
       })
-      .catch(error => {
-        console.error('Error fetching user image: ', error);
+      .catch((error) => {
+        console.error('error', error);
       });
   };
+
 
   useEffect(() => {
-    fetchUserData();
+    axios.get(`http://localhost:5002/user/${userId}`)
+      .then((response) => setUserData(response.data))
+      .catch((error) => console.error(error));
   }, [userId]);
 
-  const handleSaveDataChanges = () => {
-    axios.put(`http://localhost:5002/user/${userId}`, {
-      username: newUserData.username,
-      email: newUserData.email,
-      password: newUserData.password
-    })
-      .then(response => {
-        alert('Data changes saved successfully');
-        // Update the component's state to reflect the saved changes
-        setUserData(newUserData);
-      })
-      .catch(error => {
-        console.error('Error saving data changes: ', error);
-      });
-  };
-  
-  
+  const isDataModified = JSON.stringify(newUserData) !== JSON.stringify(userData);
 
-  const handleSaveImageChanges = () => {
-    const formData = new FormData();
-    formData.append('image', newUserData.image);
 
-    axios.post(`http://localhost:4000/upload/${userId}`, formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    })
 
-      .catch((error) => {
-        console.error('Error saving image changes: ', error);
-      });
-  };
 
-  const handleImageChange = (e) => {
-    setNewUserData({ ...newUserData, image: e.target.files[0] });
-
-    setUserImage(URL.createObjectURL(e.target.files[0]));
-  };
 
   return (
     <div>
       <div className="sm">
         <div className="text-center p-4">
           <img
-            src={userImage || "https://img.freepik.com/premium-vector/avatar-icon002_750950-52.jpg?size=338&ext=jpg&ga=GA1.1.1880011253.1699056000&semt=ais"}
+            src={userData?.image || "https://img.freepik.com/premium-vector/avatar-icon002_750950-52.jpg?size=338&ext=jpg&ga=GA1.1.1880011253.1699056000&semt=ais"}
             alt="User Image"
             className="h-32 w-32 rounded-full mx-auto"
           />
-          <span className="font-medium text-gray-900">{userData.name}</span>
-          <span className="text-gray-500">{userData.email}</span>
+
+          <span className="font-medium text-gray-900">{userData ? userData.username : 'User Name'}</span>
+          <p><span className="text-gray-500">{userData ? userData.email : 'Email'}</span></p>
         </div>
       </div>
 
-
-
-
       <ul className="text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
         <li className="w-full">
-          <a href="#" onClick={() => setActiveTab('EditProfile')} className={`inline-block w-full p-4 ${activeTab === 'EditProfile' ? 'text-gray-900 bg-gray-100' : 'bg-white hover:text-gray-700 hover-bg-gray-50'} rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white`} aria-current={activeTab === 'EditProfile' ? 'page' : null}>
+          <a
+            href="#"
+            onClick={() => setActiveTab('EditProfile')}
+            className={`inline-block w-full p-4 ${activeTab === 'EditProfile' ? 'text-gray-900 bg-gray-100' : 'bg-white hover:text-gray-700 hover-bg-gray-50'} rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white`}
+            aria-current={activeTab === 'EditProfile' ? 'page' : null}
+          >
             Edit Profile
           </a>
         </li>
 
         <li className="w-full">
-          <a href="#" onClick={() => setActiveTab('OrderHistory')} className={`inline-block w-full p-4 ${activeTab === 'OrderHistory' ? 'bg-white hover-text-gray-700 hover-bg-gray-50' : 'dark-bg-gray-800 dark-hover-text-white dark-hover-bg-gray-700'} focus-ring-4 focus-ring-blue-300 focus-outline-none`} aria-current={activeTab === 'OrderHistory' ? 'page' : null}>
+          <a
+            href="#"
+            onClick={() => setActiveTab('OrderHistory')}
+            className={`inline-block w-full p-4 ${activeTab === 'OrderHistory' ? 'bg-white hover-text-gray-700 hover-bg-gray-50' : 'dark-bg-gray-800 dark-hover-text-white dark-hover-bg-gray-700'} focus-ring-4 focus-ring-blue-300 focus-outline-none`}
+            aria-current={activeTab === 'OrderHistory' ? 'page' : null}
+          >
             Order History
           </a>
         </li>
+
         <li className="w-full">
-          <a href="#" onClick={() => setActiveTab('WishList')} className={`inline-block w-full p-4 ${activeTab === 'WishList' ? 'bg-white hover-text-gray-700 hover-bg-gray-50' : 'dark-bg-gray-800 dark-hover-text-white dark-hover-bg-gray-700'} rounded-r-lg focus-ring-4 focus-outline-none focus-ring-blue-300`} aria-current={activeTab === 'WishList' ? 'page' : null}>
+          <a
+            href="#"
+            onClick={() => setActiveTab('WishList')}
+            className={`inline-block w-full p-4 ${activeTab === 'WishList' ? 'bg-white hover-text-gray-700 hover-bg-gray-50' : 'dark-bg-gray-800 dark-hover-text-white dark-hover-bg-gray-700'} rounded-r-lg focus-ring-4 focus-outline-none focus-ring-blue-300`}
+            aria-current={activeTab === 'WishList' ? 'page' : null}
+          >
             Wish List
           </a>
         </li>
@@ -121,57 +95,55 @@ const Profile = () => {
       {activeTab === 'EditProfile' && (
         <div>
           <div className="flex justify-center mt-20 px-8">
-            <form className="max-w-2xl" encType="multipart/form-data" action="/update-profile" method="post">
-              <div className="flex flex-wrap border shadow rounded-lg p-3 dark:bg-gray-600">
+            <form
+              className="max-w-2xl"
+              encType="multipart/form-data"
+              action="/update-profile"
+              method="post"
+            >
+              <div className="flex flex-wrap border shadow rounded-lg p-3 dark-bg-gray-600">
                 <h2 className="text-xl text-gray-600 dark:text-gray-300 pb-2">Account settings:</h2>
 
                 <div className="flex flex-col gap-2 w-full border-gray-400">
                   <div>
                     <label className="text-gray-600 dark:text-gray-400">User name</label>
                     <input
-                      className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow dark-bg-gray-600 dark:text-gray-100"
+                      className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow dark-bg-gray-600 dark-text-gray-100"
                       type="text"
-                      value={newUserData.username} // Update value to newUserData.username
-                      onChange={e => setNewUserData({ ...newUserData, username: e.target.value })} // Set newUserData.username on change
+                      value={newUserData.username}
+                      onChange={(e) => setNewUserData({ ...newUserData, username: e.target.value })}
                     />
+                  </div>
 
-                  </div>
                   <div>
-                    <label className="text-gray-600 dark:text-gray-400">Password</label>
+                    <label className="text-gray-600 dark-text-gray-400">Email</label>
                     <input
-                      className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow dark-bg-gray-600 dark:text-gray-100"
-                      type="password"
-                    />
-                  </div>
-                  <div>
-                    <label className="text-gray-600 dark:text-gray-400">Email</label>
-                    <input
-                      className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow dark-bg-gray-600 dark:text-gray-100"
+                      className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow dark-bg-gray-600 dark-text-gray-100"
                       type="email"
+                      value={newUserData.email}
+                      onChange={(e) => setNewUserData({ ...newUserData, email: e.target.value })}
                     />
                   </div>
+
                   <div>
-                    <label className="text-gray-600 dark:text-gray-400">Profile Picture</label>
+                    <label className="text-gray-600 dark-text-gray-400">Password</label>
                     <input
-                      className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow dark-bg-gray-600 dark:text-gray-100"
-                      type="file"
-                      name="image"
+                      className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow dark-bg-gray-600 dark-text-gray-100"
+                      type="password"
+                      value={newUserData.password}
+                      onChange={(e) => setNewUserData({ ...newUserData, password: e.target.value })}
                     />
                   </div>
+
+
                   <div className="flex justify-end">
                     <button
-                      onClick={handleSaveDataChanges}
-                      className="py-1.5 px-3 m-1 text-center bg-gray-800 border rounded-md text-white hover:bg-gray-800 hover:text-gray-100 dark-text-gray-200 dark-bg-violet-700"
+                      onClick={handleSaveChanges}
+                      className={`py-1.5 px-3 m-1 text-center ${isDataModified ? 'bg-gray-800' : 'bg-gray-400'
+                        } border rounded-md text-white hover:bg-gray-800 hover-text-gray-100 dark-text-gray-200 dark-bg-violet-700`}
                       type="button"
                     >
-                      Save Data Changes
-                    </button>
-                    <button
-                      onClick={handleSaveImageChanges}
-                      className="py-1.5 px-3 m-1 text-center bg-gray-800 border rounded-md text-white hover:bg-gray-800 hover-text-gray-100 dark-text-gray-200 dark-bg-violet-700"
-                      type="button"
-                    >
-                      Save Image Changes
+                      Save changes
                     </button>
                   </div>
                 </div>
@@ -186,6 +158,6 @@ const Profile = () => {
       {activeTab === 'WishList' && <a />}
     </div>
   );
-}
+};
 
 export default Profile;
